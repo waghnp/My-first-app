e70dac9d664e12fe44b044d649d525565055115c
waghnp <nwagh436@gmail.com>
2020-08-19 23:31:25 +0530

    my first page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import {Navbar,NavabarBrand, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Navbar dark color='success'>
             <div className='container'>
               <NavbarBrand href='/'>Welcome back</NavbarBrand>
             </div>
         </Navbar>
+        <Menu/>
     </div>
   );
 }
