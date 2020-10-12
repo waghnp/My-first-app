0d50275248e6f81200e902810b1ee86a40533941
waghnp <nwagh436@gmail.com>
2020-10-12 10:14:22 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb,Button,Label,Row, BreadcrumbItem,Modal, ModalHeader, ModalBody } from 'reactstrap';
 import {Control,LocalForm,Errors} from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -92,8 +93,8 @@ class CommentForm extends Component{
 
 
   function RenderDish({dish})
-        {
-            if(dish != null){
+        {        
+           if (dish != null) {
                return(
                 <div className="col-md-5 col-12 m-1">
                     <Card>
@@ -141,29 +142,53 @@ class CommentForm extends Component{
       
  
       const DishDetail=(props)=>{
-        return(
-            <div className="container">
-                    <div className="row">
-                        <Breadcrumb>
-
-                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className="col-12">
-                            <h3>{props.dish.name}</h3>
-                            <hr />
-                        </div>                
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
                     </div>
-                <div className=" row" >
-                    <RenderDish dish={props.dish}/>
-                    <RenderComments  comments={props.comments}
-                        addComment={props.addComment}
-                        dishId={props.dish.id}
-                    />
                 </div>
-            </div>
-            
-        )
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) {
+            return(
+                <div className="container">
+                        <div className="row">
+                            <Breadcrumb>
+    
+                                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <div className="col-12">
+                                <h3>{props.dish.name}</h3>
+                                <hr />
+                            </div>                
+                        </div>
+                    <div className=" row" >
+                        <RenderDish dish={props.dish} />
+                        <RenderComments  comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id}
+                        />
+                    </div>
+                </div>
+                
+            )
+        }else{
+            return(
+                <div></div>
+            )
+        }
 
       }
      
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,8 +1,16 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionsTypes from './ActionsTypes';
 
-export const Dishes=(state=DISHES,action )=>{
+
+export const Dishes=(state={ isLoading:true,errMess:null,dishes:[]},action )=>{
     switch(action.type){
+        case ActionsTypes.ADD_DISHES:
+            return {...state,isLoading:false,errMess:null,dishes:action.payload};
+        case ActionsTypes.DISHES_LOADING:
+            return {...state,isLoading:true,errMess:null,dishes:[]};
+        case ActionsTypes.DISHES_FAILED:
+            return {...state,isLoading:false,errMess:action.payload};
         default:
             return state;
     }
 }
+
