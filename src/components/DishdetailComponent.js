ef214e7b7caef9d5f0dcdc3f3b9b17f3fe4c1ebf
waghnp <nwagh436@gmail.com>
2020-10-16 11:11:00 +0530

    fetch from the server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb,Button,Label,Ro
 import {Control,LocalForm,Errors} from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -98,7 +99,7 @@ class CommentForm extends Component{
                return(
                 <div className="col-md-5 col-12 m-1">
                     <Card>
-                        <CardImg top width="100%"  src={dish.image} alt={dish.name}/>
+                        <CardImg top width="100%"  src={ baseUrl+dish.image} alt={dish.name}/>
                         <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,7 +1,13 @@
-import { PROMOTIONS } from '../shared/promotions';
+import * as ActionsTypes from './ActionsTypes';
 
-export const Promotions=(state=PROMOTIONS,action )=>{
+export const Promotions=(state={ isLoading:true,errMess:null,promotions:[]},action )=>{
     switch(action.type){
+        case ActionsTypes.ADD_PROMOS:
+            return {...state,isLoading:false,errMess:null,promotions:action.payload};
+        case ActionsTypes.PROMOS_LOADING:
+            return {...state,isLoading:true,errMess:null,promotions:[]};
+        case ActionsTypes.PROMOS_FAILED:
+            return {...state,isLoading:false,errMess:action.payload};
         default:
             return state;
     }
